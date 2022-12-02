8d0566b739a5f034f2ce093f3ee8aafd7213dbbf
KAMATAGOB <97806237+KAMATAGOB@users.noreply.github.com>
2022-12-02 16:59:53 +0100

    added code to conversion funktions

--- Praktikum4/support.cpp
+++ Praktikum4/support.cpp
@@ -3,14 +3,14 @@
 //
 #include "support.h"
 
-int convertToInt(char input[]) {//imp write this shit
+int convertToInt(char input[]) {//imp extend this shit
     int number = 0;
 
     number = atoi(input);
     return number;
 }
 
-double convertToDouble(char input[]) {//imp write this shit
+double convertToDouble(char input[]) {//imp extend this shit
     double number = 0;
 
     number = atof(input);
