63531c9d95dbbb4294591a4d7fe19f5b126cfa9b
KAMATAGOB <97806237+KAMATAGOB@users.noreply.github.com>
2022-12-03 19:57:16 +0100

    added funktion komments

--- Praktikum4/support.h
+++ Praktikum4/support.h
@@ -1,4 +1,4 @@
-//
+//supports the main funktion contains a converter funktion
 // Created by Johannes Harnisch on 01.12.22.
 //
 
@@ -9,7 +9,12 @@
 #include <cstring>
 #include <cctype>
 
-
+/// <summary>
+/// take an array of char and checks if conversion to double is possible and converts to double
+/// </summary>
+/// <param name="input">input to be converted, array of char, c.b.r.</param>
+/// <param name="out">the var in which the value is written, double, c.b.r. </param>
+/// <returns>is it possible to convert the input to double, bool, c.b.v. </returns>
 bool convertToDouble(char input[], double &out);
 
 #endif //PRAKTIKUM4_SUPPORT_H
